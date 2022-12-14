78163fee6f297138a6184f1dcadcb46d847ef598
titobi <37884436+titobi@users.noreply.github.com>
2022-12-14 13:45:32 +0100

    Add files via upload

--- Ads2.py
+++ Ads2.py
@@ -11,6 +11,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#For the purpose of this analysis, we would define a function
+
+# A function to read in files in the Worldbank format returning original and transposedformat
 def stats(filename,countries,columns,indicator):
     df = pd.read_csv(filename,encoding='latin1')
     df = df[df['Indicator Name'] == indicator]
@@ -20,7 +23,7 @@ def stats(filename,countries,columns,indicator):
     return df,df.transpose()
 
 filename = 'Series_metadata.csv'
-countries = ['Ireland','Croatia','Jordan','Libya']
+countries = ['Ireland','Angola','Jordan','Libya']
 columns = ['Country Name', '2010','2011','2012','2013','2014','2015','2016','2017','2018','2019']
 indicators = ['CO2 emissions (kt)', 'Arable land (% of land area)', 'Forest area (% of land area)','GDP per capita growth (annual %)']
 
@@ -29,9 +32,13 @@ cnty_land,year_land = stats(filename,countries,columns,indicators[1])
 cnty_forest,year_forest = stats(filename,countries,columns,indicators[2])
 cnty_gdp,year_gdp = stats(filename,countries,columns,indicators[3])
 
+#The describe() method computes and displays summary statistics for the following
 print(cnty_co2.describe())
+print(year_co2.describe())
 
 year_co2 = year_co2.astype(float)
+
+#We plot a line graph for the Year on Year Trend of the CO2 Emission for these 4 countries
 plt.figure(figsize=(11,7),dpi=500)
 for i in range(len(countries)):
     plt.plot(year_co2.index,year_co2[countries[i]],label=countries[i])
@@ -42,6 +49,8 @@ plt.legend()
 plt.show()
 
 year_gdp = year_gdp.astype(float)
+
+#We plot a line graph for the Year on Year Trend of the GDP for these 4 countries
 plt.figure(figsize=(10,7),dpi=500)
 for i in range(len(countries)):
     plt.plot(year_gdp.index,year_gdp[countries[i]],label=countries[i])
@@ -51,8 +60,9 @@ plt.ylabel('GDP')
 plt.legend()
 plt.show()
 
+#we want to plot the heat map for 2 countries, to do that we have to the correlation for them
 
-
+#We want to do the correlation for Jordan to enable us do the heat map
 Jordan = pd.DataFrame(
  {'C02 emission': year_co2['Jordan'].astype(float),
  'Arable land': year_land['Jordan'].astype(float),
@@ -62,31 +72,42 @@ Jordan = pd.DataFrame(
 
 print(Jordan.corr())
 
+#We plot the heat map for Jordan to figure out the correlation between the indicators
 plt.figure(figsize=(8,5))
 sns.heatmap(Jordan.corr(),annot=True,cmap='Greens')
-plt.title('Correlation heatmap Jordan')
+plt.title('Correlation heatmap for Jordan')
 plt.show()
 
-Croatia = pd.DataFrame(
- {'C02 emission': year_co2['Croatia'].astype(float),
- 'Arable land': year_land['Croatia'].astype(float),
- 'Forest Area': year_forest['Croatia'].astype(float),
- 'GDP': year_gdp['Croatia'].astype(float)},
+#We want to do the correlation for Croatia to enable us do the heat map
+Angola = pd.DataFrame(
+ {'C02 emission': year_co2['Angola'].astype(float),
+ 'Arable land': year_land['Angola'].astype(float),
+ 'Forest Area': year_forest['Angola'].astype(float),
+ 'GDP': year_gdp['Angola'].astype(float)},
  ['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019'])
 
-print(Croatia.corr())
+print(Angola.corr())
 
+#eE plot the heat map for Jordan to figure out the correlation between the indicators
 plt.figure(figsize=(8,5))
-sns.heatmap(Croatia.corr(),annot=True,cmap='Greens')
-plt.title('Correlation heatmap Croatia')
+sns.heatmap(Angola.corr(),annot=True,cmap='Greens')
+plt.title('Correlation heatmap For Angola')
 plt.show()
 
-
+#We will now plot a bar chart for the forest area in the countries over the years
 cnty_forest =  cnty_forest.astype(float)
 cnty_forest.plot(kind='bar')
-plt.title('Grouped bar for Forest on for different countries over the years')
+plt.title('Grouped bar for Forest Area in the countries over the years')
 plt.xlabel('Countries')
 plt.ylabel('Forest Area')
 plt.rcParams["figure.dpi"] = 500
 plt.show()
 
+#We will now plot a bar chart for the Arable Land in the countries over the years
+cnty_land =  cnty_land.astype(float)
+cnty_land.plot(kind='bar')
+plt.title('Grouped bar for Arable Land in the countries over the years')
+plt.xlabel('Countries')
+plt.ylabel('Arable Land')
+plt.rcParams["figure.dpi"] = 500
+plt.show()
